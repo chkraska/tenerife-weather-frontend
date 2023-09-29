1d797af346964ed8de6c0e7952f228798278c0da
chkraska <kraska.chrystian@gmail.com>
2023-09-29 11:31:05 +0200

    Tabelka

---
+++ src/components/TableRow/TableRow.js
@@ -0,0 +1,19 @@
+import React from "react";
+import styles from "./TableRow.module.css";
+export default function TableRow({ tr }) {
+  console.log(tr);
+  const date = tr.timestamp.slice(4, 21);
+  return (
+    <>
+      <tr>
+        <td>{date}</td>
+        <td>{tr.temp}</td>
+        <td>{tr.min_temp}</td>
+        <td>{tr.max_temp}</td>
+        <td>{tr.humidity}</td>
+        <td>{tr.clouds}</td>
+        <td>{tr.desc}</td>
+      </tr>
+    </>
+  );
+}
--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,29 +1,47 @@
-import React, { useEffect, useState } from 'react'
-import Header from '../../components/Header/Header'
-import WeatherCard from '../../components/WeatherCard/WeatherCard'
+import React, { useEffect, useState } from "react";
+import Header from "../../components/Header/Header";
+import WeatherCard from "../../components/WeatherCard/WeatherCard";
+import styles from "../HomePage/HomePage.module.css";
+import TableRow from "../../components/TableRow/TableRow";
 
 export default function HomePage() {
-  const [weathers, setWeathers] = useState([])
+  const [weathers, setWeathers] = useState([]);
 
-  useEffect(()=>{
-    getWeathers()
-  },[])
+  useEffect(() => {
+    getWeathers();
+  }, []);
 
   const getWeathers = async () => {
-    const response = await fetch("http://localhost:5000/api/pogoda")
-    const {data} = await response.json()
-    setWeathers(data)
-  }
+    const response = await fetch("http://localhost:5000/api/pogoda");
+    const { data } = await response.json();
+    setWeathers(data);
+  };
 
-  console.log(weathers)
+  console.log(weathers);
 
   return (
     <div>
-        <Header/>
-        <h1>Dane pogodowe</h1>
-        <div>
-           {weathers.map((weather) => <WeatherCard item={weather}/>)}
-        </div>
+      <Header />
+      <h1>Dane pogodowe</h1>
+      <div>
+        {weathers.map((weather) => (
+          <WeatherCard item={weather} />
+        ))}
+        <table className={styles.table}>
+          <tr>
+            <th>Data: </th>
+            <th>Temperatura: </th>
+            <th>Minimalna Temperatura:</th>
+            <th>Maxymalna Temperatura:</th>
+            <th>Wilgotność: </th>
+            <th>Zachumurzenie: </th>
+            <th>Opis: </th>
+          </tr>
+          {weathers.map((weather) => (
+            <TableRow tr={weather} />
+          ))}
+        </table>
+      </div>
     </div>
-  )
+  );
 }
