54323607bbe7c9c83b9ea8cb5efb0655003e06b7
chkraska <kraska.chrystian@gmail.com>
2023-09-29 10:46:03 +0200

    Dodanie stylu

--- src/components/WeatherCard/WeatherCard.js
+++ src/components/WeatherCard/WeatherCard.js
@@ -1,13 +1,32 @@
-import React from 'react'
+import React from "react";
+import styles from "./WeatherCard.module.css";
+export default function WeatherCard({ item }) {
+  console.log(item);
 
-export default function WeatherCard({item}) {
-
-  console.log(item)
-
-  const date = item.timestamp.slice(4,21)
+  const date = item.timestamp.slice(4, 21);
   return (
-    <div>
+    <div className={styles.container}>
+      <div>
         <p>Data: {date}</p>
+      </div>
+      <div>
+        <p>Temperatura: {item.temp}</p>
+      </div>
+      <div>
+        <p>Minimalna Temperatura: {item.min_temp}</p>
+      </div>
+      <div>
+        <p>Maxymalna Temperatura: {item.max_temp}</p>
+      </div>
+      <div>
+        <p>Wilgotność: {item.humidity}</p>
+      </div>
+      <div>
+        <p>Zachumurzenie: {item.clouds}</p>
+      </div>
+      <div>
+        <p>Opis: {item.desc}</p>
+      </div>
     </div>
-  )
+  );
 }
