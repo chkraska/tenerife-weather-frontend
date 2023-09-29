10c0bdf1d29c6d538f0a70446ef8827861a18d61
dawid <dawtom97@gmail.com>
2023-09-29 08:27:19 +0200

    react router dom setup

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import HomePage from './pages/HomePage/HomePage'
+import ContactPage from './pages/ContactPage/ContactPage'
 
 export default function App() {
   return (
-    <div>App</div>
+    <BrowserRouter>
+       <Routes>
+          <Route path="/" element={<HomePage/>}/>
+          <Route path="/kontakt" element={<ContactPage/>}/>
+       </Routes>
+    </BrowserRouter>
   )
 }
 
