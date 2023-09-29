bdf4f458797751a1def030a7f8662177c8416cdc
oskgar2000 <oskgarbien@gmail.com>
2023-09-29 08:56:46 +0200

    dodanie route o-nas, dete

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import React from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import HomePage from './pages/HomePage/HomePage'
 import ContactPage from './pages/ContactPage/ContactPage'
-
+import AboutPage from './pages/AboutPage/AboutPage'
+import DetailsPage from './pages/DetailsPage/DetailsPage'
 export default function App() {
   return (
     <BrowserRouter>
        <Routes>
           <Route path="/" element={<HomePage/>}/>
           <Route path="/kontakt" element={<ContactPage/>}/>
+          <Route path="/o-nas" element={<AboutPage/>}/>
+          <Route path="/detale/:id" element={<DetailsPage/>}/>
        </Routes>
     </BrowserRouter>
   )
