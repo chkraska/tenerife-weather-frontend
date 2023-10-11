3ff0c7fec6dabb6228643927bc3a12d102d2c0b1
chkraska <kraska.chrystian@gmail.com>
2023-10-11 15:47:11 +0200

    Updated AboutPage

--- src/components/VerticalBarChart/VerticalBarChart.js
+++ src/components/VerticalBarChart/VerticalBarChart.js
@@ -75,3 +75,4 @@ export function VerticalBarChart({ weathers }) {
 
   return <Bar options={options} data={data} />;
 }
+
--- src/pages/AboutPage/AboutPage.js
+++ src/pages/AboutPage/AboutPage.js
@@ -6,6 +6,16 @@ export default function AboutPage() {
     <div>
       <Header />
       <h1>O nas</h1>
+      <h3>Witaj!</h3>
+      <p>
+        Jesteśmy kursantami gdzie wspólnie stworzyliśmy ten projekt wraz z
+        naszym prowadzącym Dawidem.
+      </p>
+      <p>
+        Projekt ten ma na celu pobierać dane pogodowe na Teneryfie i co 10 minut
+        je aktualizować i wyświetlać.
+      </p>
+      <p>Głowynm celem tego projektu była dogłebna nauka frameworka React.</p>
     </div>
   );
 }
